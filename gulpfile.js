f8f45131d4a6bff0a61a374e09cff0532321d7d9
Nick Vincent <the.nick.vincent@gmail.com>
2017-08-15 22:30:28 -0700

    gulp-sass init

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+  gulp.src('app/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('default', function() {
+  gulp.watch('app/sass/**/*.scss', ['styles']);
+});
