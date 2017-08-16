3a6da928663a19ce26f8d8685057c60420521b66
Nick Vincent <the.nick.vincent@gmail.com>
2017-08-15 22:34:11 -0700

    gulp-postcss & autoprefixer init

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 gulp.task('styles', function() {
   gulp.src('app/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer({
+        browsers: ['last 2 version']
+      })
+    ]))
     .pipe(gulp.dest('./dist/css/'));
 });
 
