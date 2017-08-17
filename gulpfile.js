fe06b7001b6d75fdfd225f822516be502691c64c
Nick Vincent <the.nick.vincent@gmail.com>
2017-08-16 20:12:24 -0700

    Add browser-sync to run server & live reload

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,15 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  })
+})
 
 gulp.task('styles', function() {
   gulp.src('app/sass/styles.scss')
@@ -11,9 +20,13 @@ gulp.task('styles', function() {
         browsers: ['last 2 version']
       })
     ]))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['browserSync', 'styles'], function() {
   gulp.watch('app/sass/**/*.scss', ['styles']);
+  gulp.watch('dist/*.html', browserSync.reload);
 });
